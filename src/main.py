eac8380116b8e1a7d70091cdd09669369073d056
Liam Frappell <liamfrappell@Liams-MacBook-Pro.local>
2022-11-14 17:58:42 +1100

    Finished working on ebl_read. Work to be done still, but reading the entire dataset now.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ __license__ = "MIT"
 __version__ = "0.0.1"
 __debug_mode__ = False
 
-
 import argparse
 from pathlib import Path
 import re
@@ -25,7 +24,6 @@ def debug_mode(header: dict) -> None:
     )
     print(f"Header 4: {header_4_check}, read {header_4_read-header_3_read}/14 bytes.")
     print(f"Data Size Valid: {data_size_valid}, read {actual_data_size} bytes.")
-
     return None
 
 def debug(input: str):
@@ -93,7 +91,7 @@ def dummy_file():
     test_file['header_3']['filename'] = 'C\x004\x00 \x00L\x00F'
     return test_file
 
-def mod_read_ebl_file(input_file: Path, error_dir: Path):
+def read_ebl_file(input_file: Path, error_dir: Path):
     file = {"filename": input_file.name, "path": input_file, "read": 0, "size": 0}
     with open(input_file, mode="rb") as file_reader:
         file_reader.seek(0, 2)
@@ -131,7 +129,7 @@ def mod_read_ebl_file(input_file: Path, error_dir: Path):
         if header_3_padding > 0:
             file['padding'] = header_3_padding
             file_reader.read(header_3_padding)
-            debug(f"{file['read']} After Header 3, should be {file['header_3']['data']}, needing {header_3_padding} bytes of padding.")
+            #debug(f"{file['read']} After Header 3, should be {file['header_3']['data']}, needing {header_3_padding} bytes of padding.")
         else:
             file['padding'] = 0
         
@@ -163,164 +161,57 @@ def mod_read_ebl_file(input_file: Path, error_dir: Path):
                                 'comment': (byte := file_reader.read(64)),
                                 'read': file_reader.tell() - file['read']
         }
-        file['read'] = file_reader.tell()
 
-        file['header_read'] = file['read']
-        
-        debug(f"HEADER READ: {file['header_read']}")
-        debug(f"v2: {file['header_data']['v2']}, pad: {file['padding']}")
-        data_padding = file['header_data']['v2'] - 180 + file['padding']
-        debug(f"FILE PADDING: {data_padding}")
+        # This block was revealed to me in a dream
+        file['channel_1_size'] = (file['header_data']['v3'] - file['header_data']['v2'])
+        file['channel_2_size'] = (file['header_data']['v5'] - file['header_data']['v4'])  
+
+        if file['channel_1_size'] == file['channel_2_size']:
+            #debug("Channels same Length")
+            if file['channel_1_size'] == 0:
+                #debug(f"MONO DETECTED.")
+                file['channel_1_size'] = file['header_data']['v4'] - file['header_data']['v3'] + 2
+                file['channel_2_size'] = 0
+        else:
+            debug("Error: Channels Different length.")
+
+        file['data_size_calc'] = file['channel_1_size'] + file['channel_2_size']
+
+        #data_padding = file['header_data']['v2'] - 180 + file['padding'] # v1
+        #data_padding = file['header_data']['v2'] - 180 # + file['padding'] # v2
+        data_padding = file['header_data']['v5'] - file['data_size_calc'] - 178 # v3. Probably can read 178 somewhere.
         if data_padding > 0:
+            #debug(f"READ {data_padding} bytes of data padding!")
             file_reader.read(data_padding)
-            debug(f"READ {data_padding} bytes of data padding!")
-        
-        # How much data is in each channel? We minus 4 to avoid the empty byte.
-        file['channel_1_size'] = (file['header_data']['v4'] + 2 - file['header_data']['v2'])
-        file['channel_2_size'] = (file['header_data']['v5'] + 2 - file['header_data']['v3'])
 
-        file['channel_2_size'] = 0 if file['header_data']['v2'] == file['header_data']['v3'] else file['channel_2_size']
-        
-        debug(f"C1: {file['channel_1_size']} C2: {file['channel_2_size']}")
-
-        file['data_size'] = file['size'] - file['header_read']
-
-        #print(file)
-
-        file['channel_1_data'] = file_reader.read(file['channel_1_size'])
-        #if file['channel_1_size'] > 0:
-        #data_padding = int.from_bytes(
-        #    byte := file_reader.read(4), "little"
-        #)  # Should be zeros all the time. LOL
-        file['channel_2_data'] = file_reader.read(file['channel_2_size'])
-        #print()
-        #print(file['channel_2_size'])
-        #print()
-        #debug(f"C1: {file['channel_1_size']}, C2: {file['channel_2_size']}")
-        end_of_data = file_reader.tell()
-        debug(f"{end_of_data}:{file['size']}")
-    return file
-
-def read_ebl_file(input_file: Path, error_dir: Path):
-    file = {"filename": input_file.name, "path": input_file, "read": 0, "size": 0}
-    with open(input_file, mode="rb") as file_reader:
-        file_reader.seek(0, 2)
-        file['size'] = file_reader.tell()
-        file_reader.seek(0, 0)
-
-        # Preliminary File Header is 8 bytes.
-        file['header_1'] = {
-                                "prefix": (byte := file_reader.read(4)), # "FORM"
-                                "filesize": int.from_bytes(byte := file_reader.read(4), "big"), # FileSize - 8 (i.e how many bytes are left)
-                                "read": file_reader.tell()
-                            }
-        file['read'] = file_reader.tell()
-
-        # Header 2 just contains a size of metadata field. 12 bytes.
-        file['header_2'] = {
-                                "prefix": (byte := file_reader.read(8)),  # "E5B0TOC2"
-                                "next_header_bytes": int.from_bytes(byte := file_reader.read(4), "big"), # Length of the next Chunk??? 78.
-                                "read": file_reader.tell() - file['read']
-                            }
-        file['read'] = file_reader.tell()
-
-        # Header 3 just contains the filename, and an updated metadata size and filesize for something different. 78 bytes (From header_2_data)
-        file['header_3'] = {
-                                "prefix": (byte := file_reader.read(4)), # "E5S1"
-                                "data_size": int.from_bytes(byte := file_reader.read(4), "big"), # 343480. The Size after "header_4_data" below, i.e byte >= 108
-                                "data": int.from_bytes(byte := file_reader.read(4), "big"),  # ??? 98
-                                "zeros": file_reader.read(2), # 0's here. No idea why.
-                                "filename": (byte := file_reader.read(64)).decode("utf-8"), # The following 64 bytes are the track name, more or less encoded utf-8.
-                                "read": file_reader.tell() - file['read']
-                            }
-        file['read'] = file_reader.tell()
-
-        # Another E5S1 header. 14 bytes. No idea why.
-        file['header_4'] = {
-                                'prefix': (byte := file_reader.read(4)),  # "E5S1"
-                                'size': int.from_bytes(byte := file_reader.read(4), "big"), # 343482
-                                'data': (byte := file_reader.read(6)), # 256 be, 1 le
-                                'read': file_reader.tell() - file['read']
-                            }
-        file['read'] = file_reader.tell()
-
-        # Start of Data Chunk 2? 184 bytes to go till start of file_reader.
-        # Need to read this properly... Unknown if static sizes.
-        file['header_data'] = {
-                                'filename': (byte := file_reader.read(64)), # The file name repeated. 64 bytes.
-                                'v1': int.from_bytes(byte := file_reader.read(4), "little"), # Unknown. 301 le
-                                'v2': int.from_bytes(byte := file_reader.read(4), "little"), # Data Offset. 184 le,
-                                'v3': int.from_bytes(byte := file_reader.read(4), "little"), # Data size (including offset). 171832 le. Aka channel 1 is 171832-184 = 171648 bytes.
-                                'v4': int.from_bytes(byte := file_reader.read(4), "little"), # Data size - 2 (Not sure why?). 171830
-                                'v5': int.from_bytes(byte := file_reader.read(4), "little"), # Close to the end of file_reader. 343478.
-                                'v6': int.from_bytes(byte := file_reader.read(4), "little"), # Chanel 1 Data Offset. 184.
-                                'v7': int.from_bytes(byte := file_reader.read(4), "little"), # Data size (including offset). 171832
-                                'v8': int.from_bytes(byte := file_reader.read(4), "little"), # 184. Start of Audio Data?
-                                'v9': int.from_bytes(byte := file_reader.read(4), "little"), # 171832. End of data for this channel?
-                                'frequency': int.from_bytes(byte := file_reader.read(4), "little"),# Frequency. Typically 44100 (hz)
-                                'v11': int.from_bytes(byte := file_reader.read(4), "little"),# 0. Unknown.
-                                'v12': int.from_bytes(byte := file_reader.read(4), "little"),# Unknown but maybe number of channels, bitrate idk.
-                                'padding': file_reader.read(72),
-                                'read': file_reader.tell() - file['read']
-        }
         file['read'] = file_reader.tell()
         file['header_read'] = file['read']
 
-        # How much data is in each channel? We minus 4 to avoid the empty byte.
-        file['channel_1_size'] = (file['header_data']['v4'] - 186)
-        file['channel_2_size'] = (file['header_data']['v5'] - file['header_data']['v4'] - 4)
-
-        file['channel_2_size'] = 0 if file['channel_2_size'] == -4 else file['channel_2_size']
-        
-        file['data_size'] = file['size'] - file['header_read']
-
-        #print(file)
+        file['data_size_est'] = file['size'] - file['header_read']
 
         file['channel_1_data'] = file_reader.read(file['channel_1_size'])
-        #if file['channel_1_size'] > 0:
-        data_padding = int.from_bytes(
-            byte := file_reader.read(4), "little"
-        )  # Should be zeros all the time. LOL
         file['channel_2_data'] = file_reader.read(file['channel_2_size'])
-        #print()
-        #print(file['channel_2_size'])
-        #print()
-        debug(f"C1: {file['channel_1_size']}, C2: {file['channel_2_size']}")
+
         end_of_data = file_reader.tell()
-
-        header_1_check = True if file['header_1']['prefix'] == b"FORM" else False
-        header_1_valid = True if file['header_1']['filesize'] == file['size'] - file['header_1']['read'] else False # Checks if the remaining bytes in header 1 is correct.
-
-        header_2_check = True if file['header_2']['prefix'] == b"E5B0TOC2" else False
-
-        header_3_check = True if file['header_3']['prefix'] == b"E5S1" else False
-        header_3_valid = True if file['header_3']['read'] == file['header_2']['next_header_bytes'] else False
-        header_3_zeros = True if file['header_3']['zeros'] == b"\x00\x00" else False
-
-        header_4_check = True if file['header_4']['prefix'] == b"E5S1" else False
-
-        header_sum = file['header_1']['read'] + file['header_2']['read'] + file['header_3']['read'] + file['header_4']['read'] + file['header_data']['read']
-        #all_headers = True if file['header_read'] = 
-
-        #header_fx_check = True if file['header_4']['v10'] == 44100 else False
-        #data_size_valid = True if file['data_size'] == (file['channel_1_size'] + file['channel_2_size']) + 4 else False
-        data_size_valid = True if end_of_data == file['size'] else False
-
-        checks_passed = header_1_check and header_1_valid and header_2_check and header_3_check and header_3_valid and header_3_zeros and header_4_check and data_size_valid
-        if not checks_passed:
-            debug(f"\n\n--- DEBUG: EBL_READ, FILE: {input_file.name}, FP: {file['read']}")
-            debug(f"H1: FORM Prefix: {header_1_check}, FILE SIZE: {header_1_valid}, READ: {file['header_1']['read']}b")
-            debug(f"H2: E5B0TOC2 Prefix: {header_2_check}, READ: {file['header_2']['read']}b")
-            debug(f"H3: E5S1 Prefix: {header_3_check}, VALID: {header_3_valid}, ZEROS: {header_3_zeros}, DATA: {file['header_3']['data']}, READ: {file['header_3']['read']}b")
-            debug(f"H4: E5S1 Prefix: {header_4_check}, SIZE: {file['header_4']['size']}, DATA: {file['header_4']['data']}READ: {file['header_4']['read']}b")
-            debug(f"HD: FX: {file['header_data']['frequency']}, READ: {file['header_data']['read']}b")
-            debug(f"ALL HEADERS: SIZE: {header_sum}, READ: {file['header_read']}b")
-            debug(f"DATA: SIZE_VALID: {data_size_valid}, REMAINING: {file['size'] - end_of_data}b, READ: {end_of_data}")
-
+        file['read'] = file_reader.tell()
+        #debug(f"Data Size: {file['header_read']}")
+        #debug(f"Padding: {data_padding}")
+        #debug(f"Header Read: {testing_header_read}, Padding: {file['header_read']}")
+        #debug(f"C1: {file['channel_1_size']}, C2: {file['channel_2_size']}")
+        #debug(f"C1: {c1}, C2: {c2}")
+        #debug(input_file.name)
+        #debug(f"C1 s: {file['channel_1_data'][0:8]}")
+        #debug(f"WARN: Inconsistent filesize: Read: {end_of_data}, Expected: {file['size']}, Difference: {file['size']-end_of_data}")
+        
+        if end_of_data != file['size']:
+            if (file['size']-end_of_data) != 40:
+                debug(f"WARN: Inconsistent filesize: Read: {end_of_data}, Expected: {file['size']}, Difference: {file['size']-end_of_data}")
+            else:
+                debug("WARN: Found 40 bytes. Additional data header.")
     return file
 
 def write_wav(input_file: Path, output_dir: Path, ebl_file):
-    #ebl_file = test_file
+    #ebl_file = dummy_file()
 
     wav_header_length = 16
     wav_pcm_mode = 1
@@ -353,8 +244,8 @@ def write_wav(input_file: Path, output_dir: Path, ebl_file):
     if vars(args)['no_write']:
         print('Not Writing to disk...')
     else:
-        #try:
-        if True:
+        try:
+        #if True:
             if vars(args)['preserve_filename']:
                 output_file = input_file.stem + '.wav'
             else:
@@ -404,20 +295,20 @@ def write_wav(input_file: Path, output_dir: Path, ebl_file):
                     wav_data_size.to_bytes(4, byteorder="little")
                 )  # Write Size of actual audio...
                 file_writer.write(wav_data)
-        #except:
-        #    print(f"Failed to write {output_file}")
+        except:
+            print(f"Failed to write {output_file}")
     return
 
 def convert_file(input_file: Path, output_dir: Path, error_dir: Path):
     try:
-        #ebl_file = read_ebl_file(input_file, error_dir)
-        ebl_file = mod_read_ebl_file(input_file, error_dir)
+        ebl_file = read_ebl_file(input_file, error_dir)
     except:
         print(f'EBL READ ERROR: {input_file.name}')
         if vars(args)['error_save']:
             Path(error_dir, input_file.name).write_bytes(input_file.read_bytes())
     else:
         write_wav(input_file, output_dir, ebl_file)
+        
 
 def main(input_dir, output_dir, args):
     """Main entry point of the app"""
@@ -443,9 +334,9 @@ if __name__ == "__main__":
     """This is executed when run from the command line"""
     parser = argparse.ArgumentParser()
 
-    default_input_dir = Path.cwd().joinpath("input_3")
+    default_input_dir = Path.cwd().joinpath("input_4")
     #default_input_dir = Path("src/input/test.ebl") # Testing Single File Input
-    default_output_dir = Path.cwd().joinpath("output_3")
+    default_output_dir = Path.cwd().joinpath("output_4")
 
     # input_dir = parser.add_argument("input_dir", action="store")
     input_dir = default_input_dir
